8ee70740b8f179d000e285403dc2583e9fcbb477
davquipe <davidcondori99@gmail.com>
2018-06-16 00:11:01 -0500

    update in profile.component.html | /profile.component.ts | preview of the selected  image

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -12,6 +12,7 @@ export class ProfileComponent implements OnInit {
   usuario: Usuario;
 
   imagenSubir: File;
+  imagenTemp: string;
 
   constructor(
     public _usuarioService: UsuarioService
@@ -39,7 +40,18 @@ export class ProfileComponent implements OnInit {
       return;
     }
 
+    if ( archivo.type.indexOf('image') < 0 ) {
+      swal( 'Sólo Imagenes', 'El archivo que selecciono no es una imagen', 'error' );
+      this.imagenSubir = null;
+      return;
+    }
+
     this.imagenSubir = archivo;
+
+    let reader = new FileReader();
+    let urlImagenTemp = reader.readAsDataURL( archivo );
+
+    reader.onloadend = () => this.imagenTemp = reader.result;
 
   }
 
